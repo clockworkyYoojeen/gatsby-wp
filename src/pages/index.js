0e5b1589b37f3d5975b06579842aa10070d1905b
yoojeen <sendtoeugeen@gmail.com>
2024-03-03 21:51:05 +0300

    Latest Blog posts created

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Layout from "../components/Layout/Layout"
 import SEO from "../components/SEO/SEO"
 import Hero from "../components/Hero/Hero"
 import CTAArea from "../components/CTAArea/CTAArea"
+import LatestBlogPost from "../components/LatestBlogPost/LatestBlogPost"
 
 
 
@@ -13,6 +14,7 @@ const IndexPage = () => (
     <h4>Start developing lightning fast frontend for WordPress sites with Gatsby!</h4>
     <Hero />
     <CTAArea />
+    <LatestBlogPost />
   </Layout>
 )
 
